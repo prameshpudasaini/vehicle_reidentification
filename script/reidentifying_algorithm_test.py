85b7b5eb181af0734f537c27de45c764dffafc6b
prameshpudasaini <prameshpudasaini@gmail.com>
2023-10-01 19:17:41 -0700

    Updated algorithm

--- script/reidentifying_algorithm_test.py
+++ script/reidentifying_algorithm_test.py
@@ -94,12 +94,24 @@ def evaluatePerformance(param_model):
     y_pred_test_full = param_model.predict(X_test)
 
     # evaluation metrics on test set
-    mape_test = mean_absolute_percentage_error(y_test, y_pred_test_full)
-    rmse_test = np.sqrt(mean_squared_error(y_test, y_pred_test_full))
+    mape_test_full = mean_absolute_percentage_error(y_test, y_pred_test_full)
+    rmse_test_full = np.sqrt(mean_squared_error(y_test, y_pred_test_full))
     
     print("Model with hypertuned parameters and trained on training + validation datasets")
-    print("MAPE on test set: {:.4f}".format(mape_test))
-    print("RMSE on test set: {:.4f}".format(rmse_test), "\n")
+    print("MAPE on test set: {:.4f}".format(mape_test_full))
+    print("RMSE on test set: {:.4f}".format(rmse_test_full), "\n")
+    
+    # make predictions on candidate adv for matching
+    X_adv_candidate = cdf.drop(['file', 'adv', 'stop', 'match', 'travel_time'], axis = 1)
+    y_pred_adv_candidate = param_model.predict(X_adv_candidate)
+    
+    return {'mape_valid': mape_valid,
+            'rmse_valid': rmse_valid,
+            'mape_test': mape_test,
+            'rmse_test': rmse_test,
+            'mape_test_full': mape_test_full,
+            'rmse_test_full': rmse_test_full,
+            'y_pred_adv_candidate': y_pred_adv_candidate}
 
 # =============================================================================
 # Ridge Regression
@@ -118,9 +130,14 @@ ridge_param_grid = {'alpha': [1e-15, 1e-10, 1e-5, 0.001, 0.01, 0.1, 1, 2, 5, 10,
 ridge_grid_result = performGridSearch(ridge_reg, ridge_param_grid)
 
 # specify model with tuned parameters
-param_ridge_reg = Ridge(**ridge_grid_result['best_params'])
+ridge_reg_tuned = Ridge(**ridge_grid_result['best_params'])
 
-evaluatePerformance(param_ridge_reg)
+# evaluate performance
+ridge_reg_result = evaluatePerformance(ridge_reg_tuned)
+
+# add predicted travel time values
+cdf_ridge = cdf.copy(deep = True)
+cdf_ridge['y_pred'] = ridge_reg_result['y_pred_adv_candidate']
 
 # =============================================================================
 # reidentifying algorithm
@@ -134,94 +151,99 @@ match_ground = match_ground[['file', 'adv', 'stop']]
 tt_thru_min = 2.5
 tt_thru_max = 12
 
-# function to predict travel time for individual vehicle actuation over adv det
-def predictTravelTime(file, id_adv):
-    # filter feature vector X for adv ID
-    X_i = cdf.copy(deep = True)[(cdf.file == file[:-4]) & (cdf.adv == id_adv)] # discard .txt
-    
-    # drop columns redundant for travel time prediction
-    drop_cols = ['file', 'adv', 'stop', 'match', 'travel_time']
-    X_i.drop(drop_cols, axis = 1, inplace = True)
-    
-    tt_predict = param_ridge_reg.predict(X_i)[0] # predicted travel time
+# function to get predicted travel time for individual vehicle actuation over adv det
+def predictTravelTime(df_pred_tt, file, id_adv):
+    X_i = df_pred_tt.copy(deep = True)[(df_pred_tt.file == file[:-4]) & (df_pred_tt.adv == id_adv)] # discard .txt
+    X_i.reset_index(drop = True, inplace = True)
+    tt_predict = X_i.loc[0, 'y_pred'] # predicted travel time
     return tt_predict
 
 # processed files to run through reidentifying algorithm
 file_path = "ignore/data_ground_truth/processed"
 files = os.listdir(file_path)
 
-# store reidentified match pairs from each file
-df_result = []
-
-for file in files:
-    print("Running reidentification algorithm for file: ", file)
-    # read events-processed file with timestamp data
-    df = pd.read_csv(os.path.join(file_path, file), sep = '\t')
-    df.TimeStamp = pd.to_datetime(df.TimeStamp, format = '%Y-%m-%d %H:%M:%S.%f').sort_values()
-    df.dropna(axis = 0, inplace = True) # drop rows with Nan
+def reidentifyAlgorithm(df_pred_tt):
+    df_result = [] # store reidentified match pairs from each file
     
-    # data frames for adv and stop-bar det
-    adf = df[df.Det == 'adv']
-    sdf = df[df.Det == 'stop']
-    
-    # actuation IDs
-    id_adv = list(sorted(adf.ID))
-    id_stop = list(sorted(sdf.ID))
-    
-    thru_match_initial = [] # store initial candidate match pairs of adv to stop-bar det
-    
-    for i in id_adv:
-        adv_time = adf[adf.ID == i].TimeStamp.values[0]
-        adv_lane = adf[adf.ID == i].Lane.values[0]
+    for file in files:
+        print("Running reidentification algorithm for file: ", file)
+        # read events-processed file with timestamp data
+        df = pd.read_csv(os.path.join(file_path, file), sep = '\t')
+        df.TimeStamp = pd.to_datetime(df.TimeStamp, format = '%Y-%m-%d %H:%M:%S.%f').sort_values()
+        df.dropna(axis = 0, inplace = True) # drop rows with Nan
         
-        # stop-bar det IDs on the same lane to look for a match
-        id_stop_look = set(sdf[sdf.Lane == adv_lane].ID)
+        # data frames for adv and stop-bar det
+        adf = df[df.Det == 'adv']
+        sdf = df[df.Det == 'stop']
+        id_adv = list(sorted(adf.ID))
         
-        for j in id_stop_look:
-            stop_time = sdf[sdf.ID == j].TimeStamp.values[0]
+        thru_match_initial = [] # store initial candidate match pairs of adv to stop-bar det
+        
+        for i in id_adv:
+            adv_time = adf[adf.ID == i].TimeStamp.values[0]
+            adv_lane = adf[adf.ID == i].Lane.values[0]
             
-            if stop_time > adv_time: # look forward in timestamp
-                tt_adv_stop = (stop_time - adv_time) / np.timedelta64(1, 's') # paired travel time
+            # stop-bar det IDs on the same lane to look for a match
+            id_stop_look = set(sdf[sdf.Lane == adv_lane].ID)
             
-                if tt_thru_min <= tt_adv_stop <= tt_thru_max:
-                    tt_predict = predictTravelTime(file, i) # predicted travel time
-                    tt_diff = round(abs(tt_adv_stop - tt_predict), 4) # abs diff between paired & predicted
-                    
-                    # store adv ID, stop ID, travel time diff
-                    thru_match_initial.append([i, j, tt_diff])
-    
-    # dicts to store the lowest error for each adv, stop ID
-    seen_adv_id, seen_stop_id = {}, {}
-    
-    # iterate through each candidate pair
-    for pair in thru_match_initial:
-        adv_id, stop_id, error = pair
+            for j in id_stop_look:
+                stop_time = sdf[sdf.ID == j].TimeStamp.values[0]
+                
+                if stop_time > adv_time: # look forward in timestamp
+                    tt_adv_stop = (stop_time - adv_time) / np.timedelta64(1, 's') # paired travel time
+                
+                    if tt_thru_min <= tt_adv_stop <= tt_thru_max:
+                        tt_predict = predictTravelTime(df_pred_tt, file, i) # predicted travel time
+                        tt_diff = round(abs(tt_adv_stop - tt_predict), 4) # abs diff between paired & predicted
+                        
+                        # store adv ID, stop ID, travel time diff
+                        thru_match_initial.append([i, j, tt_diff])
         
-        # check if adv ID not seen or if error is lower than seen error for that adv ID
-        if (adv_id not in seen_adv_id) or (error < seen_adv_id[adv_id][1]):
-            seen_adv_id[adv_id] = list([stop_id, error])
+        # dicts to store the lowest error for each adv, stop ID
+        seen_adv_id, seen_stop_id = {}, {}
         
-        # check if stop ID not seen or if error is lower than seen error for that stop ID
-        if (stop_id not in seen_stop_id) or (error < seen_stop_id[stop_id][1]):
-            seen_stop_id[stop_id] = list([adv_id, error])
+        # iterate through each candidate pair
+        for pair in thru_match_initial:
+            adv_id, stop_id, error = pair
+            
+            # check if adv ID not seen or if error is lower than seen error for that adv ID
+            if (adv_id not in seen_adv_id) or (error < seen_adv_id[adv_id][1]):
+                seen_adv_id[adv_id] = list([stop_id, error])
+            
+            # check if stop ID not seen or if error is lower than seen error for that stop ID
+            if (stop_id not in seen_stop_id) or (error < seen_stop_id[stop_id][1]):
+                seen_stop_id[stop_id] = list([adv_id, error])
+        
+        # match pairs for adv with lowest error
+        df_adv = pd.DataFrame(seen_adv_id, index = ['adv', 'stop']).T.reset_index()
+        df_adv.columns = ['adv', 'stop', 'error']
+        
+        # match pairs for stop with lowest error
+        df_stop = pd.DataFrame(seen_stop_id, index = ['stop', 'adv']).T.reset_index()
+        df_stop.columns = ['stop', 'adv', 'error']
+        
+        # resulting common match pairs
+        df_match_pair = df_adv.merge(df_stop, on = ['adv', 'stop', 'error'])
+        df_match_pair['file'] = file[:-4]
+        df_result.append(df_match_pair)
+        
+    match_result = pd.concat(df_result)
     
-    # match pairs for adv with lowest error
-    df_adv = pd.DataFrame(seen_adv_id, index = ['adv', 'stop']).T.reset_index()
-    df_adv.columns = ['adv', 'stop', 'error']
+    # get true positive (TP), false positive (FP), and false negative (FN) matches
+    match_cols = ['file', 'adv', 'stop']    
+    match_TP = pd.merge(match_result, match_ground, on = match_cols)
+    match_TP['class'] = 'TP'
+    match_FP = match_result.merge(match_ground, on = match_cols, how = 'left', indicator = True).query('_merge == "left_only"').drop(columns = '_merge')
+    match_FP['class'] = 'FP'
+    match_FN = match_ground.merge(match_result, on = match_cols, how = 'left', indicator = True).query('_merge == "left_only"').drop(columns = '_merge')
+    match_FN['class'] = 'FN'
     
-    # match pairs for stop with lowest error
-    df_stop = pd.DataFrame(seen_stop_id, index = ['stop', 'adv']).T.reset_index()
-    df_stop.columns = ['stop', 'adv', 'error']
+    match_result_class = pd.concat([match_TP, match_FP, match_FN])
+    match_result_metrics = {'TP': match_TP.shape[0],
+                            'FP': match_FP.shape[0],
+                            'FN': match_FN.shape[0]}
     
-    # resulting common match pairs
-    df_match_pair = df_adv.merge(df_stop, on = ['adv', 'stop', 'error'])
-    df_match_pair['file'] = file[:-4]
-    df_result.append(df_match_pair)
-    
-match_result = pd.concat(df_result)
+    return {'match_result_class': match_result_class,
+            'match_result_metrics': match_result_metrics}
 
-# get true positive (TP), false positive (FP), and false negative (FN) matches
-match_cols = ['file', 'adv', 'stop']
-match_TP = pd.merge(match_result, match_ground, on = match_cols)
-match_FP = match_result.merge(match_ground, on = match_cols, how = 'left', indicator = True).query('_merge == "left_only"').drop(columns = '_merge')
-match_FN = match_ground.merge(match_result, on = match_cols, how = 'left', indicator = True).query('_merge == "left_only"').drop(columns = '_merge')
+ridge_result = reidentifyAlgorithm(cdf_ridge)
